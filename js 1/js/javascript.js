d3e9499e28606e811cf1a9347aefb72d7caefc9e
Dmitrii Barasii <dmitrii.barasii@gmail.com>
2017-10-08 01:11:24 +0300

    js homework

---
+++ js 1/js/javascript.js
@@ -0,0 +1,9 @@
+alert(document.getElementById("one").innerHTML);
+
+var a =document.getElementById("two");
+a.innerHTML='hello word';
+alert(a.innerHTML);
+
+var i="Hello!!!";
+document.getElementById("ttt").innerHTML=
+    document.getElementById("ttt").innerHTML=i;
