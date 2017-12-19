745381f59d58bc0c318bef48c31d5bd5810b0ee3
Dmitrii Barasii <dmitrii.barasii@gmail.com>
2017-12-19 04:35:50 +0200

    fix hw

--- js 3/js/javascript.js
+++ js 3/js/javascript.js
@@ -1,31 +1,45 @@
 let p = document.getElementById("arrayOut");
 let inputInformation = document.getElementById('inputInformation');
 
-function f10() {
-    let x = inputInformation.value;
-    return x;
+// function f10() {
+//     let x = inputInformation.value;
+//     return x;
+// }
+function getValue (input) {
+    return input.value;
 }
+getValue (inputInformation);
 
 let m = [1,9,3,7,99,100,66];
 console.log(m);
 
-function massOut() {
-    // let p = document.getElementById("arrayOut");
-    let str = '';
+// function massOut() {
+//     // let p = document.getElementById("arrayOut");
+//     let str = '';
+//
+//     for(let i=0; i<m.length; i++){
+//         str += m[i] + ', ';
+//     }
+//     p.innerHTML=str;
+// }
+// massOut();
 
-    for(let i=0; i<m.length; i++){
-        str += m[i] + ', ';
+function massOut(arr, elem) {
+    let str = '';
+    for(let i=0; i < arr.length; i++){
+        str += arr[i] + ', ';
     }
-    p.innerHTML=str;
+    elem.textContent = str;
 }
-massOut();
+massOut(m, p);
+
 
 let pushBtn = document.getElementById('pushBtn');
 pushBtn.addEventListener('click', push, false);
 
 function push() {
     // let inputInformation = document.getElementById('inputInformation').value;
-    m.push(f10());
+    m.push(getValue());
     massOut();
 }
 
@@ -52,6 +66,6 @@ function unshift() {
     // let inputInformation = document.getElementById('inputInformation').value;
 
     // m.unshift(inputInformation.value);
-    m.unshift(f10());
+    m.unshift(getValue());
     massOut();
 }
